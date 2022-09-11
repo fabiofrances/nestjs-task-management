028facd6b6a9d74103283266791b8746f5e5636d
Fabio Franceschetto <fabiof.frances@gmail.com>
2022-09-11 19:16:56 -0300

    create auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDTO } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
+import { UsersReporitory } from './users.repository';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(UsersReporitory)
+    private usersRepository: UsersReporitory,
+  ) {}
+
+  async signUp(authCredentialsDTO: AuthCredentialsDTO): Promise<void> {
+    return this.usersRepository.createUser(authCredentialsDTO);
+  }
+}
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,7 +1,16 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDTO } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
 @EntityRepository(User)
 export class UsersReporitory extends Repository<User> {
-  //..
+  async createUser(authCredentiasDTO: AuthCredentialsDTO): Promise<void> {
+    const { username, password } = authCredentiasDTO;
+    const newUser = this.create({
+      username,
+      password,
+    });
+    await this.save(newUser);
+    // return newUser;
+  }
 }
