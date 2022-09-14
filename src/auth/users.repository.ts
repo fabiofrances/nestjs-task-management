5add2b4ea4af96aa9373d218025d9516aec5ce5f
Fabio Franceschetto <fabiof.frances@gmail.com>
2022-09-13 22:08:29 -0300

    add jwt strategy for protected routes

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -8,7 +8,7 @@ import { User } from './user.entity';
 import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
-export class UsersReporitory extends Repository<User> {
+export class UsersRepository extends Repository<User> {
   async createUser(authCredentiasDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentiasDto;
 
